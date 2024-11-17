6b3f9c990e646230193bae32738735257a6ccda6
Okekeifeanyi <okekeifeanyi20@yahoo.com>
2024-11-17 17:18:12 +0100

    generation page

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import GeneratePaCode from "./pages/csPages/GeneratePaCodePage";
 import EnrolleePaCode from "./pages/csPages/EnrolleePaCodePage";
 import ApprovePaCode from "./pages/csPages/ApprovePaCodePage";
 import SuccessGen from "./pages/csPages/SuccessGeneratePaPage";
+//import Provider from "./pages/csPages/providersPages/ProviderPage";
 
 function App() {
   return (
@@ -43,6 +44,7 @@ function App() {
         <Route path="/enrolleePaCode" element={<EnrolleePaCode />} />
         <Route path="/approvePaCode" element={<ApprovePaCode />} />
         <Route path="/successGen" element={<SuccessGen />} />
+        {/* <Route path="/provider" element={<Provider />} />  */}
       </Routes>
     </BrowserRouter>
   );
