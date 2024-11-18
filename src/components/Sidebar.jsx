c70578f78e144a5d34b426768bdb671f52229424
Fawazboluwatife7 <147808555+Fawazboluwatife7@users.noreply.github.com>
2024-11-18 12:08:07 +0100

    sidebar  added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Sidebar = () => {
+    const [outerSideBar, setouterSideBar] = useState(null);
+    const [innerSideBar, setinnerSideBar] = useState(null);
+
+    const handleCLick = (item) => {
+        setouterSideBar(item);
+    };
     return (
-        <div className=" w-[40%] flex">
-            <div className=" w-[10%] bg-[#121638] h-[100vh] col-auto justify-items-center ">
-                <div className=" cursor-pointer mt-8 group hover:bg-[#C61531] p-2 ">
+        <div className=" w-[18%]  bg-black overflow-x-hidden top-0 left-0 fixed flex">
+            <div className=" w-[22%] bg-[#121638] h-[100vh] col-auto justify-items-center ">
+                <div
+                    className=" cursor-pointer mt-8 group hover:bg-[#C61531] p-2 "
+                    onClick={setouterSideBar}
+                >
                     <img src="Underwriting@3x.svg" alt="" onClick={""} />
                 </div>
                 <div className=" cursor-pointer  hover:bg-[#C61531] p-2">
@@ -63,7 +73,7 @@ const Sidebar = () => {
                     <img src="Vector@3x.svg" alt="" onClick={""} />
                 </div>
             </div>
-            <div className=" bg-white w-[35%] border  border-red-500 h-[100vh] justify-center items-center">
+            <div className=" bg-white border w-full  border-red-500 h-[100vh] justify-center items-center">
                 <img
                     src="SmallLogo.svg"
                     alt=""
