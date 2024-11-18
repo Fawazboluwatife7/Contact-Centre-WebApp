9101a108d91ae2d4f90ae621e1269fc872d368db
Okekeifeanyi <okekeifeanyi20@yahoo.com>
2024-11-18 12:59:20 +0100

    provider first page

--- src/components/cs/Provider.jsx
+++ src/components/cs/Provider.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import search from "../../assets/CSIMAGES/Search.svg";
-import filter from "../../assets/csImages/filter.svg";
-import sort from "../../assets/csImages/sort.svg";
+import print from "../../assets/csImages/print.svg";
+import exportf from "../../assets/csImages/export.svg";
 import edit from "../../assets/CSIMAGES/pencil-edit.svg";
 import deleteicon from "../../assets/CSIMAGES/delete.svg";
+import arrow from "../../assets/CSIMAGES/bxs_sort-alt.svg"
 import { useNavigate } from "react-router-dom";
 
 function ProviderComponent() {
@@ -13,7 +14,7 @@ function ProviderComponent() {
   const [showAll, setShowAll] = useState(false);
   const navigate = useNavigate();
 
-  // Search state variables
+ 
   const [searchName, setSearchName] = useState("");
   const [searchEnrolleeId, setSearchEnrolleeId] = useState("");
   const [searchScheme, setSearchScheme] = useState("");
@@ -80,114 +81,162 @@ function ProviderComponent() {
           <label htmlFor="searchName" className="block mb-1">
             Provider Name
           </label>
-          <input
-            id="searchName"
-            type="text"
-            className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none"
-            placeholder="Search"
-            value={searchName}
-            onChange={(e) => setSearchName(e.target.value)}
-          />
+          <div className="relative">
+            {/* Positioning the search icon inside the input field */}
+            <span className="absolute inset-y-0 left-3 flex items-center">
+              <img src={search} alt="Search" className="h-5 w-5" />
+            </span>
+            <input
+              id="searchName"
+              type="text"
+              className="w-full p-2 pl-10 border-2 border-gray-300 rounded-md focus:outline-none"
+              placeholder="Search"
+              value={searchName}
+              onChange={(e) => setSearchName(e.target.value)}
+            />
+          </div>
         </div>
 
         <div className="relative">
-          <label htmlFor="searchEnrolleeId" className="block mb-1">
-            Provider Code
+          <label htmlFor="searchName" className="block mb-1">
+          Provider Code
           </label>
-          <input
-            id="searchEnrolleeId"
-            type="text"
-            className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none"
-            placeholder="Search"
-            value={searchEnrolleeId}
-            onChange={(e) => setSearchEnrolleeId(e.target.value)}
-          />
+          <div className="relative">
+            {/* Positioning the search icon inside the input field */}
+            <span className="absolute inset-y-0 left-3 flex items-center">
+              <img src={search} alt="Search" className="h-5 w-5" />
+            </span>
+            <input
+              id="searchName"
+              type="text"
+              className="w-full p-2 pl-10 border-2 border-gray-300 rounded-md focus:outline-none"
+              placeholder="Search"
+              value={searchName}
+              onChange={(e) => setSearchName(e.target.value)}
+            />
+          </div>
         </div>
 
         <div className="relative">
-          <label htmlFor="searchScheme" className="block mb-1">
-            Provider Class (e.g., Hospital, Gym, Pharmacy)
+          <label htmlFor="searchName" className="block mb-1">
+          Provider Class (e.g., Hospital, Gym, Pharmacy)
           </label>
-          <input
-            id="searchScheme"
-            type="text"
-            className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none"
-            placeholder="Search"
-            value={searchScheme}
-            onChange={(e) => setSearchScheme(e.target.value)}
-          />
+          <div className="relative">
+            {/* Positioning the search icon inside the input field */}
+            <span className="absolute inset-y-0 left-3 flex items-center">
+              <img src={search} alt="Search" className="h-5 w-5" />
+            </span>
+            <input
+              id="searchName"
+              type="text"
+              className="w-full p-2 pl-10 border-2 border-gray-300 rounded-md focus:outline-none"
+              placeholder="Search"
+              value={searchName}
+              onChange={(e) => setSearchName(e.target.value)}
+            />
+          </div>
         </div>
 
         <div className="relative">
-          <label htmlFor="searchPlanType" className="block mb-1">
+          <label htmlFor="searchName" className="block mb-1">
             Location
           </label>
-          <input
-            id="searchPlanType"
-            type="text"
-            className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none"
-            placeholder="Search"
-            value={searchPlanType}
-            onChange={(e) => setSearchPlanType(e.target.value)}
-          />
+          <div className="relative">
+            {/* Positioning the search icon inside the input field */}
+            <span className="absolute inset-y-0 left-3 flex items-center">
+              <img src={search} alt="Search" className="h-5 w-5" />
+            </span>
+            <input
+              id="searchName"
+              type="text"
+              className="w-full p-2 pl-10 border-2 border-gray-300 rounded-md focus:outline-none"
+              placeholder="Search"
+              value={searchName}
+              onChange={(e) => setSearchName(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="relative">
+          <label htmlFor="searchName" className="block mb-1">
+            Scheme
+          </label>
+          <div className="relative">
+            {/* Positioning the search icon inside the input field */}
+            <span className="absolute inset-y-0 left-3 flex items-center">
+              <img src={search} alt="Search" className="h-5 w-5" />
+            </span>
+            <input
+              id="searchName"
+              type="text"
+              className="w-full p-2 pl-10 border-2 border-gray-300 rounded-md focus:outline-none"
+              placeholder="Search"
+              value={searchName}
+              onChange={(e) => setSearchName(e.target.value)}
+            />
+          </div>
         </div>
       </div>
 
       {/* Filter and Sort */}
       <div className="flex justify-end mt-5 mb-2">
         <div className="text-red-700 w-[88px] h-[36px] flex items-center justify-center bg-white outline">
-          <img src={filter} alt="Filter" className="w-[20px] h-[20px] mr-1" /> Filter
+          <img src={print} alt="Filter" className="w-[20px] h-[20px] mr-1" />{" "}
+          Print
         </div>
         <div className="text-red-700 w-[88px] h-[36px] flex items-center justify-center bg-white outline ml-2">
-          <img src={sort} alt="Sort" className="w-[20px] h-[20px] mr-1" /> Sort
+          <img src={exportf} alt="Sort" className="w-[20px] h-[20px] mr-1" /> Export
         </div>
       </div>
 
       {/* Table Display */}
       <div className="overflow-x-auto w-full bg-white">
-        <div className="ml-6 mt-5 mb-4 text-red-700 font-bold">Provider List</div>
+        <div className="ml-6 mt-5 mb-4 text-red-700 font-bold">
+          Provider List
+        </div>
         <table className="w-full text-sm text-gray-600">
-  <thead className="bg-gray-200">
-    <tr>
-      <th className="py-2 px-4 text-left">Group Code</th>
-      <th className="py-2 px-4 text-left">Group Name</th>
-      <th className="py-2 px-4 text-left">Scheme</th>
-      <th className="py-2 px-4 text-left">Start Date</th>
-      <th className="py-2 px-4 text-left">End Date</th>
-      <th className="py-2 px-4 text-left">Status</th>
-      <th className="py-2 px-4 text-left">Actions</th>
-    </tr>
-  </thead>
-  <tbody className="font-bold">
-    {paginateData.map((item) => (
-      <tr
-        key={item.id}
-        className="border-b cursor-pointer hover:bg-gray-100"
-        onClick={() => handleRowClick(item.id)}
-      >
-        <td className="py-2 px-4">{item.groupCode}</td>
-        <td className="py-2 px-4">{item.groupName}</td>
-        <td className="py-2 px-4">{item.scheme}</td>
-        <td className="py-2 px-4">{item.startDate}</td>
-        <td className="py-2 px-4">{item.endDate}</td>
-        <td className="py-2 px-4 text-green-600 w-[58px] bg-green-100 font-bold h[20px]">{item.status}</td>
-        <td className="py-2 px-4 flex items-center gap-2">
-          <img
-            src={edit}
-            alt="Edit"
-            className="w-5 h-5 cursor-pointer"
-          />
-          <img
-            src={deleteicon}
-            alt="Delete"
-            className="w-5 h-5 cursor-pointer"
-          />
-        </td>
-      </tr>
-    ))}
-  </tbody>
-</table>
-
+          <thead className="bg-gray-200">
+            <tr>
+              <th className="py-2 px-4 text-left flex">Group Code <img src={arrow} alt="" /></th>
+              <th className="py-2 px-4 text-left">Group Name </th>
+              <th className="py-2 px-4 text-left">Scheme</th>
+              <th className="py-2 px-4 text-left">Start Date</th>
+              <th className="py-2 px-4 text-left">End Date</th>
+              <th className="py-2 px-4 text-left">Status</th>
+              <th className="py-2 px-4 text-left">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="font-bold">
+            {paginateData.map((item) => (
+              <tr
+                key={item.id}
+                className="border-b cursor-pointer hover:bg-gray-100"
+                onClick={() => handleRowClick(item.id)}
+              >
+                <td className="py-2 px-4">{item.groupCode}</td>
+                <td className="py-2 px-4">{item.groupName}</td>
+                <td className="py-2 px-4">{item.scheme}</td>
+                <td className="py-2 px-4">{item.startDate}</td>
+                <td className="py-2 px-4">{item.endDate}</td>
+                <td className="py-2 px-4 text-green-600 w-[58px] bg-green-100 font-bold h[20px]">
+                  {item.status}
+                </td>
+                <td className="py-2 px-4 flex items-center gap-2">
+                  <img
+                    src={edit}
+                    alt="Edit"
+                    className="w-5 h-5 cursor-pointer"
+                  />
+                  <img
+                    src={deleteicon}
+                    alt="Delete"
+                    className="w-5 h-5 cursor-pointer"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
       {/* Show All Toggle */}
