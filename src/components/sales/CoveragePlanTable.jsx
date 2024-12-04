447566d819d8692254dd6a3c72bfb9d41e3ce2bc
Bahdtoluwani <oladosu.toluene@gmail.com>
2024-12-04 15:26:05 +0100

    coverage plan page styling modified

--- src/components/sales/CoveragePlanTable.jsx
+++ src/components/sales/CoveragePlanTable.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const CoveragePlanTable = () => {
   const [activeTab, setActiveTab] = useState("Overview");
--- src/pages/sales/CoveragePlan.jsx
+++ src/pages/sales/CoveragePlan.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import CoveragePlanTable from '../../components/sales/CoveragePlanTable';
 
 const CoveragePlan = () => {
   return (
     <div className="container mx-auto p-2">
-      <div className="max-w-7xl mx-auto py-2 px-4 rounded-lg ">
+      <div className=" mx-auto py-2 px-4 rounded-lg ">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-2xl font-bold">Leadway Assurance - Coverage Plan</h1>
           <Link to="/SalesDashboard/clients-profile" className="text-red-500 font-bold">Back To Profile
           </Link>
         </div>
         <div className="grid grid-cols-3 gap-4 mb-14">
-        <div className="bg-gray-500 p-4 rounded-lg">
+         <div className="bg-gray-500 p-8 h-60 rounded-lg">
             <div className="flex justify-between items-center">
               <div>
                 <h2 className="text-xl font-bold text-white">Pro Max</h2>
@@ -31,24 +30,44 @@ const CoveragePlan = () => {
               <p className="text-sm text-white">Policy No: 10001234</p>
             </div>
           </div>  
-          <div className="bg-pink-400 p-4 rounded-lg">
-            <h2 className="text-xl font-bold">Max</h2>
-            <p>Leadway Assurance</p>
-            <p className="text-sm text-gray-600">Exp: Feb 28 2023</p>
-            <p className="text-sm text-gray-600">75 Principals</p>
-            <p className="text-sm text-gray-600">180 Dependents</p>
-            <p className="text-sm text-gray-600">Policy No: 10001234</p>
-            <button className="mt-4 bg-white text-gray-600 py-2 px-4 rounded">View</button>
-          </div>
-          <div className="bg-orange-400 p-4 rounded-lg">
-            <h2 className="text-xl font-bold">Pro</h2>
-            <p>Leadway Assurance</p>
-            <p className="text-sm text-gray-600">Exp: Feb 28 2023</p>
-            <p className="text-sm text-gray-600">75 Principals</p>
-            <p className="text-sm text-gray-600">180 Dependents</p>
-            <p className="text-sm text-gray-600">Policy No: 10001234</p>
-            <button className="mt-4 bg-white text-gray-600 py-2 px-4 rounded">View</button>
-          </div>
+          <div className="bg-pink-400 p-8 rounded-lg">
+            <div className="flex justify-between items-center">
+              <div>
+                <h2 className="text-xl font-bold text-white">Max</h2>
+                <p className='text-white'>Leadway Assurance</p>
+              </div>
+              <div className="text-right">
+                <button className="mt-2 bg-white text-gray-600 py-2 px-4 rounded">View</button>   
+                <p className="text-sm text-gray-600 text-white">Exp: Feb 28 2023</p> 
+              </div>
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <p className="text-sm text-white">75 Principals</p>
+                <p className="text-sm text-white">180 Dependents</p>
+              </div>
+              <p className="text-sm text-white">Policy No: 10001234</p>
+            </div>
+          </div> 
+          <div className="bg-orange-500 p-8 rounded-lg">
+            <div className="flex justify-between items-center">
+              <div>
+                <h2 className="text-xl font-bold text-white">Pro</h2>
+                <p className='text-white'>Leadway Assurance</p>
+              </div>
+              <div className="text-right">
+                <button className="mt-2 bg-white text-gray-600 py-2 px-4 rounded">View</button>   
+                <p className="text-sm text-gray-600 text-white">Exp: Feb 28 2023</p> 
+              </div>
+            </div>
+            <div className="mt-4 flex justify-between">
+              <div>
+                <p className="text-sm text-white">75 Principals</p>
+                <p className="text-sm text-white">180 Dependents</p>
+              </div>
+              <p className="text-sm text-white">Policy No: 10001234</p>
+            </div>
+          </div> 
         </div>
         <CoveragePlanTable />
       </div>
